4f39765f9a3da39ccd68c8d23654711931fc92b6
vamhana <litokol.kazan@gmail.com>
2025-11-17 01:43:25 +0300

    Update index.js

--- modules/build-script/index.js
+++ modules/build-script/index.js
@@ -1,3 +1,8 @@
+// modules/build-script/index.js
 import { buildForVercel } from './build-processor.js';
 
-buildForVercel();
+// Запускаем сборку
+buildForVercel().catch(error => {
+    console.error('❌ Build process failed:', error);
+    process.exit(1);
+});
