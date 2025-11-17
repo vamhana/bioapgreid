94b3ac1912e05605dd173d67e8011655d3ee570b
vamhana <litokol.kazan@gmail.com>
2025-11-17 06:44:34 +0300

    Delete modules directory

--- modules/build-script/index.js
+++
@@ -1,8 +0,0 @@
-// modules/build-script/index.js
-import { buildForVercel } from './build-processor.js';
-
-// Запускаем сборку
-buildForVercel().catch(error => {
-    console.error('❌ Build process failed:', error);
-    process.exit(1);
-});
